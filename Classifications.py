9d2a28ea725a7b2b5bb634a339f5bfe575069f6a
Tram Anh <tramanh06@gmail.com>
2015-10-28 22:30:15 +0800

    Added churner label and write to csv

--- Classifications.py
+++ Classifications.py
@@ -18,15 +18,35 @@ def churn_boolean(x):
 def add_churner_column(agg_data):
     agg_data['churner'] = agg_data.apply(churn_boolean, axis=1)
 
+def plot_attribute_graph():
+    # fig = plt.figure()
+    agg_data.hist(bins=50)
+    plt.show()
+
+def write_to_csv(outfile):
+    agg_data.to_csv(outfile, index=False)
+
 if __name__ == "__main__":
     infile = './AggregateData/agg_original.csv'
+    outfile = './AggregateData/agg_original_wLabel.csv'
     churn_duration_str = '30 days'
     end_date_str = '2015-4-1'
+    remove_CC = True
+
+    if remove_CC:
+        infile = './AggregateData/agg_withoutCallCentre.csv'
+        outfile = './AggregateData/agg_withoutCallCentre_label.csv'
 
     print 'Read in '+infile+' ...'
     agg_data = pd.read_csv(infile, parse_dates=['first recds', 'last recds', 'last call',
                                                 'last sms', 'last idd', 'last activity'])
-    # Churners
+    # Churners  NOTE: NOT NEEDED IF USE agg_data_avgCallDuration.csv
     add_churner_column(agg_data)
+
+    # Show distribution graph
+    plot_attribute_graph()
+
+    # Write to csv
+    write_to_csv(outfile)
 
     # Classification
