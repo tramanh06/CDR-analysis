f4be7a4f62839632cab94a39524eeab6e08c1919
Tram Anh <tramanh06@gmail.com>
2016-03-07 00:18:25 +0800

    Fixed csv writerow error

--- encode_for_graph.py
+++ encode_for_graph.py
@@ -21,19 +21,18 @@ print 'Encode CDR with index...'
 with open(datafile, 'rb') as csvfile:
     csvreader = csv.reader(csvfile, delimiter="|")
     with open(outfile, 'wb') as csvwrite:
-        writer = csv.writer(csvwrite, quoting=csv.QUOTE_NONE)
-        writer.writerow("A_NUMBER|B_NUMBER|EVENT_TYPE|EVENT_DATE|DURATION|EVENT_COST")
+        fieldname = ["A_NUMBER","B_NUMBER","EVENT_TYPE","EVENT_DATE","DURATION","EVENT_COST"]
+        writer = csv.writer(csvwrite, quoting=csv.QUOTE_NONE, delimiter="|")
+        writer.writerow(fieldname)
         for line in csvreader:
-            if "A_NUMBER" in line:
-                # writer.write(line)
+            if 'A_NUMBER' in line:
                 continue
             if line[0] and line[1]:
                 a, b, event, date, duration, cost = line[0], line[1], line[2], \
                                                     line[3], line[4], line[5]
                 a_encode = caller_dict[a]
                 b_encode = caller_dict[b]
-                writer.writerow("{0}|{1}|{2}|{3}|{4}|{5}".format(a_encode, b_encode,
-                                                              event, date, duration, cost))
+                writer.writerow([a_encode, b_encode, event, date, duration, cost])
 
 
 
