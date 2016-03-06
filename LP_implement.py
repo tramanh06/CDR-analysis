289d1d26cb8f2d1516acba8a8bf3ce21f8f51ba2
Tram Anh <tramanh06@gmail.com>
2016-03-06 23:56:56 +0800

    Refactor to function

--- LP_implement.py
+++ LP_implement.py
@@ -11,6 +11,22 @@ EVENT = {'INCOMING_CALL':0, 'OUTGOING_CALL':1, 'IDD_CALL':2, 'OUTGOING_SMS':4, '
 
 infile = './Data/cleaned_data_2.csv'
 
+def prepare_data(input_file):
+    # read in csv
+    # Convert 'EVENT_DATE' column to Timestamp
+    # Convert 'DURATION' to timedelta
+    raw_data = pd.read_csv(infile, sep='|', parse_dates=['EVENT_DATE'])
+    raw_data['DURATION'] = pd.to_timedelta(raw_data['DURATION'])
+
+    # Split into months
+    first_month = 10
+    monthly_data = []
+    for i in range(6):
+        month = (first_month + i - 1) % 12 + 1
+        monthly_data.append(raw_data[raw_data['EVENT_DATE'].dt.month == month])
+
+    return monthly_data
+
 def feature_engineer(data):
     ### Return aggreate feature in the form of dataframe
     group = data.groupby('A_NUMBER')
@@ -226,38 +242,28 @@ def set_graph_features(data0, data1):
 
 # Main program
 
-# read in csv
-# Convert 'EVENT_DATE' column to Timestamp
-# Convert 'DURATION' to timedelta
-raw_data = pd.read_csv(infile, sep='|', parse_dates=['EVENT_DATE'])
-raw_data['DURATION'] = pd.to_timedelta(raw_data['DURATION'])
+if __name__=='__main__':
+    monthly_data = prepare_data(infile)
 
-# Split into months
-first_month = 10
-monthly_data = []
-for i in range(6):
-    month = (first_month + i - 1) % 12 + 1
-    monthly_data.append(raw_data[raw_data['EVENT_DATE'].dt.month == month])
+    churn_period = monthly_data[2][monthly_data[2]['EVENT_DATE'].dt.day <16]
 
-churn_period = monthly_data[2][monthly_data[2]['EVENT_DATE'].dt.day <16]
+    print 'Creating features for training'
+    feature_data = feature_engineer(monthly_data[1])
 
-print 'Creating features for training'
-feature_data = feature_engineer(monthly_data[1])
+    print 'Add graph features. Build graph from CDR and add churner&influence label'
+    graph_data = set_graph_features(monthly_data[0], monthly_data[1])
 
-print 'Add graph features. Build graph from CDR and add churner&influence label'
-graph_data = set_graph_features(monthly_data[0], monthly_data[1])
+    # Create training set
+    print 'Graph data:'
+    print graph_data
 
-# Create training set
-print 'Graph data:'
-print graph_data
+    print 'Feature data:'
+    print feature_data
 
-print 'Feature data:'
-print feature_data
+    feature_data = feature_data.join(graph_data)
 
-feature_data = feature_data.join(graph_data)
+    print 'Determine churner label'
+    train_data = set_label(feature_data, churn_period)
 
-print 'Determine churner label'
-train_data = set_label(feature_data, churn_period)
-
-print 'Write to csv'
-train_data.to_csv('Month2_LP.csv', index=False)
+    print 'Write to csv'
+    train_data.to_csv('Month2_LP.csv', index=False)
