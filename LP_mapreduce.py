80de8b8eadab6f8a1064edf2bbeb2890b98537e3
Tram Anh <tramanh06@gmail.com>
2016-03-07 16:06:06 +0800

    copied code from example.py

--- LP_mapreduce.py
+++ LP_mapreduce.py
@@ -1,50 +1,20 @@
-__author__ = 'TramAnh'
-
-import pandas as pd
+#!/usr/bin/env python
 import mincemeat
-import cPickle as pickle
 
-from graph_construction import graph_file
-
-
-
-
-G = pickle.load( open( graph_file, 'rb' ))
-
-datasource = dict(enumerate(G.nodes()))
-
+data = ["Humpty Dumpty sat on a wall",
+        "Humpty Dumpty had a great fall",
+        "All the King's horses and all the King's men",
+        "Couldn't put Humpty together again",
+        ]
+# The data source can be any dictionary-like object
+datasource = dict(enumerate(data))
 
 def mapfn(k, v):
-    '''
-    Map function for map reduce
-    :param k: index from enumerate(nodes)
-    :param v: node number
-    :return: updated class distribution
-    '''
-    # TODO: optimize to make use of nodes for key
-    def build_classDistribution(G):
-        dict = {}
-        for n in G.nodes():
-            dict[n] = [G.node[n]['churner'], G.node[n]['influence']]
-        return dict
-
-    prob_distribution_dict = build_classDistribution(G)
-    
-    def get_neighbors(G, n):
-        return G.neighbors(n)
-
-    d = 0
-    neighbors = get_neighbors(G,v)
-
-    for neighbor in neighbors:
-        w = G[v][neighbor]['DURATION_SEC']
-        d += w * prob_distribution_dict[neighbor]
-
-    norm = [float(i)/sum(d) for i in d]
-    yield v, norm
+    for w in v.split():
+        yield w, 1
 
 def reducefn(k, vs):
-    result = vs
+    result = sum(vs)
     return result
 
 s = mincemeat.Server()
@@ -54,4 +24,3 @@ s.reducefn = reducefn
 
 results = s.run_server(password="changeme")
 print results
-
