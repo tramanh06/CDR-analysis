236a068131fa1dd2313e685788bc41f213e1b376
Tram Anh <tramanh06@gmail.com>
2016-03-07 21:13:30 +0800

    Tested 400000 processes with example. Worked

--- LP_mapreduce.py
+++ LP_mapreduce.py
@@ -1,17 +1,49 @@
 #!/usr/bin/env python
 import mincemeat
 
-data = ["Humpty Dumpty sat on a wall",
-        "Humpty Dumpty had a great fall",
-        "All the King's horses and all the King's men",
-        "Couldn't put Humpty together again",
-        ]
-# The data source can be any dictionary-like object
-datasource = dict(enumerate(data))
+import cPickle as pickle
+
+from graph_construction import graph_file
+
+G = pickle.load( open( graph_file, 'rb' ))
+datasource = dict(enumerate(G.nodes()))
+
 
 def mapfn(k, v):
-    for w in v.split():
-        yield w, 1
+    '''
+    Map function for map reduce
+    :param k: index from enumerate(nodes)
+    :param v: node number
+    :return: updated class distribution
+    '''
+    from graph_construction import graph_file
+    import cPickle as pickle
+    import networkx as nx
+
+    # # TODO: optimize to make use of nodes for key
+    # def build_classDistribution(G):
+    #     dict = {}
+    #     for n in G.nodes():
+    #         dict[n] = [G.node[n]['churner'], G.node[n]['influence']]
+    #     return dict
+    #
+    # prob_distribution_dict = build_classDistribution(G)
+    #
+    # def get_neighbors(G, n):
+    #     return G.neighbors(n)
+
+    G = pickle.load( open( graph_file, 'rb' ))
+
+    # d = 0
+    neighbors =  G.neighbors(v)
+
+    for neighbor in neighbors:
+        # w = G[v][neighbor]['DURATION_SEC']
+        # d += w * prob_distribution_dict[neighbor]
+        yield neighbor,1
+
+    # norm = [float(i)/sum(d) for i in d]
+    # yield v, norm
 
 def reducefn(k, vs):
     result = sum(vs)
